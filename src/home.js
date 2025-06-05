b78ffa7d37fb7aff12414df324e0509975a030bc
Alex Farrell <farrel22@students.rowan.edu>
2025-06-04 20:07:40 -0400

    css

--- src/home.js
+++ src/home.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
-function Home() {
-  return (
-    <div className="home-page">
-      <h1>Welcome to Personal Fitness Pal</h1>
-      <h3>Your Recommended Workouts!</h3>
-      <p>Lunges</p>
+const Navbar = () => (
+  <nav className="nav">
+    <ul>
+      <li><a href="home.html">Home</a></li>
+      <li><a href="login.html">Login</a></li>
+      <li><a href="register.html">Register</a></li>
+      <li><a href="about.html">About</a></li>
+    </ul>
+  </nav>
+);
+
+const Tile = ({ children }) => <div className="tile">{children}</div>;
+
+const WorkoutHub = () => (
+  <div className="app-container">
+    <header>Workout Hub</header>
+    <div className="grid-container">
+      <Tile>🏋 Workouts</Tile>
+      <Tile>📊 Progress Tracker</Tile>
+      <Tile>🧮 Calculators</Tile>
+      <Tile>⚙ Settings</Tile>
     </div>
-  );
-}
+  </div>
+);
 
-export default Home;
+const App = () => (
+  <>
+    <Navbar />
+    <WorkoutHub />
+  </>
+);
+
+export default App;
